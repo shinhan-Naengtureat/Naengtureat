1c30d552717364067f0d26ab157f198023d378e3
jamm0316 <jamm0316@naver.com>
2025-03-03 08:29:24 +0900

    refactor: casCade 삭제
    Cart, Inventory 엔티티는 단방향으로 설계되어있으므로 cascade가 필요 없음
    
    Resolves #9

--- backend/src/main/java/com/shinhan/naengtureat/inventory/entity/Inventory.java
+++ backend/src/main/java/com/shinhan/naengtureat/inventory/entity/Inventory.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = {"memberId", "ingredientId"})
 public class Inventory extends SuperEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
