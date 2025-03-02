f868dcc6e4ad360e0c329c49c150090ad394e72e
jamm0316 <jamm0316@naver.com>
2025-03-03 05:07:22 +0900

    feat: CartEntity 작성
    cart 엔티티 필드 및 연관관계 작성
    
    Resolves #3

--- backend/src/main/java/com/shinhan/naengtureat/member/entity/Cart.java
+++ backend/src/main/java/com/shinhan/naengtureat/member/entity/Cart.java
@@ -1,7 +1,8 @@
 package com.shinhan.naengtureat.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.shinhan.naengtureat.common.entities.SuperEntity;
+import com.shinhan.naengtureat.store.entity.StoreProduct;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -11,9 +12,19 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-
-public class Cart {
+public class Cart extends SuperEntity {
     @Id
+    @Column(name = "cart_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     int count;
+
+    boolean isCheck;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    StoreProduct productId;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    Member memberId;
 }
