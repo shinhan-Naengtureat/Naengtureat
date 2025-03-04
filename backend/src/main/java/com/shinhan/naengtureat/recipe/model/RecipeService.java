5a6429115feadcf37eefd3c2576e82e7dd566e93
Woong0405 <dbstjddnd45@gmail.com>
2025-03-04 17:25:51 +0900

    feat: pull하기위한 커밋
    Resolves #27

--- backend/src/main/java/com/shinhan/naengtureat/recipe/model/RecipeService.java
+++ backend/src/main/java/com/shinhan/naengtureat/recipe/model/RecipeService.java
@@ -1,4 +1,28 @@
 package com.shinhan.naengtureat.recipe.model;
 
+import org.springframework.stereotype.Service;
+
+import com.shinhan.naengtureat.recipe.entity.Recipe;
+import com.shinhan.naengtureat.recipe.entity.RecipeStep;
+
+@Service
 public class RecipeService {
+	
+	private RecipeRepository recipeRepository;
+	
+	private RecipeStepRepository recipeStepRepository;
+	
+	// 레시피 개요 등록
+	public Recipe insertRecipe(Recipe recipe) {
+		return recipeRepository.save(recipe);
+	}
+	
+	//레시피 재료 등록
+	
+	//레시피 순서 등록
+	public RecipeStep insertRecipeStep(RecipeStep recipeStep) {
+		return recipeStepRepository.save(recipeStep);
+	}
+	
+	//해시태그 등록
 }
