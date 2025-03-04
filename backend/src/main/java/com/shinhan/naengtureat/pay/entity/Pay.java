d5f505a85ecdee9eba228f7cede71a672128a86a
hyejin <starking999@naver.com>
2025-03-04 14:37:31 +0900

    feat: Orders, OrderDetail, Pay 엔티티 추가
    Resolves #24

--- backend/src/main/java/com/shinhan/naengtureat/pay/entity/Pay.java
+++ backend/src/main/java/com/shinhan/naengtureat/pay/entity/Pay.java
@@ -1,8 +1,22 @@
 package com.shinhan.naengtureat.pay.entity;
 
+import com.shinhan.naengtureat.common.entities.SuperEntity;
+import com.shinhan.naengtureat.member.entity.Member;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -10,8 +24,20 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-public class Pay {
-    @Id
-    Long id;
+@ToString(exclude = {"member"})
+public class Pay extends SuperEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "pay_id")
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+	
+	@Column(nullable = false)
+	private int balance;
+	
+	@Column(nullable = false)
+	private String customer_uid;
 }
