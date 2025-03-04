8b94a6e16a2a94c0f41816c58932a767870b3438
jamm0316 <jamm0316@naver.com>
2025-03-05 04:20:47 +0900

    feat: pk column명 수정
    Ingredient, Orders, OrdersDetail pk Column명 수정
    
    Resolves #40

--- backend/src/main/java/com/shinhan/naengtureat/ingredient/entity/Ingredient.java
+++ backend/src/main/java/com/shinhan/naengtureat/ingredient/entity/Ingredient.java
@@ -13,6 +13,7 @@ import lombok.*;
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_id")
     private Long id;
 
     @Column(nullable = false, length = 90)
