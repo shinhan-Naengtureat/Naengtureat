859339a4e37a13b6e190ec8dd3ee07c0e8be8cfb
GJKim-James <gjk0635@naver.com>
2025-03-04 11:35:54 +0900

    feat: Store, StoreProduct, StoreReview Entity 생성
    Store, StoreProduct, StoreReview Entity 생성
    
    Resolves #18

--- backend/src/main/java/com/shinhan/naengtureat/store/entity/Store.java
+++ backend/src/main/java/com/shinhan/naengtureat/store/entity/Store.java
@@ -1,5 +1,8 @@
 package com.shinhan.naengtureat.store.entity;
 
+import com.shinhan.naengtureat.common.entities.SuperEntity;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
@@ -11,7 +14,28 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Store {
-    @Id
-    Long id;
+public class Store extends SuperEntity {
+	@Id
+    @Column(name = "store_id")
+    private Long id; // 카카오 지도 API에서 제공하는 값 사용
+    
+    @Column(name = "store_image", nullable = false)
+    private String image; // 이미지 URL(또는 default)
+    
+    @Column(nullable = false)
+    private String placeName; // 스토어 이름
+    
+    @Column(name = "x_longitude", nullable = false)
+    private double x; // x 좌표(경도)
+    
+    @Column(name = "y_latitude", nullable = false)
+    private double y; // y 좌표(위도)
+    
+    private String phone; // 전화번호
+    
+    @Column(nullable = false)
+    private String roadAddressName; // 도로명 주소
+    
+    @Column(nullable = false)
+    private String placeUrl; // http://place.map.kakao.com/16618597
 }
