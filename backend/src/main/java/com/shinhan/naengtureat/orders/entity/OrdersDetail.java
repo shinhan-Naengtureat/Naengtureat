9938bdf1125c9a3d647faf74654d4f8a9e9caec2
jamm0316 <jamm0316@naver.com>
2025-03-04 16:05:04 +0900

    feat: 모든 entity 리펙터링
    Resolves #30

--- backend/src/main/java/com/shinhan/naengtureat/orders/entity/OrdersDetail.java
+++ backend/src/main/java/com/shinhan/naengtureat/orders/entity/OrdersDetail.java
@@ -1,24 +1,9 @@
 package com.shinhan.naengtureat.orders.entity;
 
 import com.shinhan.naengtureat.common.entities.SuperEntity;
-import com.shinhan.naengtureat.member.entity.Member;
-import com.shinhan.naengtureat.pay.entity.Pay;
-
 import com.shinhan.naengtureat.store.entity.StoreProduct;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
@@ -30,7 +15,6 @@ import lombok.ToString;
 public class OrdersDetail extends SuperEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "orders_detail_id")
     private Long id;
 
 
