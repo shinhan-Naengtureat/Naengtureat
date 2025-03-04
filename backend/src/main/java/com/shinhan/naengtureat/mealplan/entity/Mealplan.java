3b3b3456698490984e159f7dc0f6dee75d347ce2
Lee Taehan <kpl2955@gmail.com>
2025-03-04 11:17:53 +0900

    feat : Mealplan, Hashtag, RecipeHashtag 엔티티 생성
    Mealplan, Hashtag, RecipeHashtag 엔티티 생성
    
    Resolves  #16

--- backend/src/main/java/com/shinhan/naengtureat/mealplan/entity/Mealplan.java
+++ backend/src/main/java/com/shinhan/naengtureat/mealplan/entity/Mealplan.java
@@ -1,8 +1,25 @@
 package com.shinhan.naengtureat.mealplan.entity;
 
+import java.time.LocalDateTime;
+
+import com.shinhan.naengtureat.common.entities.SuperEntity;
+import com.shinhan.naengtureat.member.entity.Member;
+import com.shinhan.naengtureat.recipe.entity.Recipe;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -10,9 +27,30 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude= {"member","recipe"})
 
-public class Mealplan {
+public class Mealplan extends SuperEntity {
     @Id
-    Long id;
+    @Column(name="meal_plan_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;//식단ID
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member; //멤버번호
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id",nullable = false)
+    private Recipe recipe; // 레시피ID
+    
+    @Column(nullable = false)
+    private LocalDateTime date; // 식단일자
+    
+    @Column(nullable = false)
+    private String type; //식단분류
+    
+    @Column(nullable = false)
+    private Boolean is_check; //이행여부
+    
+
 }
