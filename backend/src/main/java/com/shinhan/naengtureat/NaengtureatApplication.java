8ac76f93eb3c917d1363aa7e97175a87e85679ae
Lee Taehan <kpl2955@gmail.com>
2025-03-05 16:04:25 +0900

    feat gptAPI 연동
    gptAPI 연동

--- backend/src/main/java/com/shinhan/naengtureat/NaengtureatApplication.java
+++ backend/src/main/java/com/shinhan/naengtureat/NaengtureatApplication.java
@@ -2,8 +2,10 @@ package com.shinhan.naengtureat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//		(exclude={DataSourceAutoConfiguration.class})
 public class NaengtureatApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/shinhan/naengtureat/common/security/AuditorAwareImpl.java
+++ backend/src/main/java/com/shinhan/naengtureat/common/security/AuditorAwareImpl.java
@@ -1,14 +1,14 @@
 package com.shinhan.naengtureat.common.security;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
+    	
         return Optional.of("system");
 
         /* springSecurity 도입 후 설정
@@ -25,4 +25,5 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     	}
          */
     }
+    
 }
