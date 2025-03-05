bfbff1073309a1cd495cf0275a13b4a6f99a0938
Woong0405 <dbstjddnd45@gmail.com>
2025-03-05 20:10:27 +0900

    [200~feat: recipe price 계산

--- backend/src/main/java/com/shinhan/naengtureat/recipe/model/RecipeService.java
+++ backend/src/main/java/com/shinhan/naengtureat/recipe/model/RecipeService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shinhan.naengtureat.ingredient.entity.Ingredient;
+import com.shinhan.naengtureat.ingredient.model.IngredientRepository;
 import com.shinhan.naengtureat.member.entity.Member;
 import com.shinhan.naengtureat.recipe.dto.RecipeDTO;
 import com.shinhan.naengtureat.recipe.dto.RecipeIngredientDTO;
@@ -35,64 +36,76 @@ public class RecipeService {
 	@Autowired
 	private RecipeHashtagRepository recipeHashtagRepository;
 	
+	@Autowired
+	private IngredientRepository ingredientRepository;
 	 
-	@Transactional
-	public void registerRecipe(RecipeDTO recipeDto, Long memberId) {
-		// 1. Recipe 생성 및 저장
-		Recipe recipe = new Recipe();
-		recipe.setName(recipeDto.getName());
-		recipe.setPrice(recipeDto.getPrice());
-		recipe.setLevel(recipeDto.getLevel());
-		recipe.setCookingTime(recipeDto.getCookingTime());
-		recipe.setServing(recipeDto.getServing());
-		recipe.setImage(recipeDto.getImage());
-		recipe.setCategory(recipeDto.getCategory());
+    @Transactional
+    public void registerRecipe(RecipeDTO recipeDto, Long memberId) {
+        // 1. Recipe 생성 및 저장
+        Recipe recipe = new Recipe();
+        recipe.setName(recipeDto.getName());
+        recipe.setLevel(recipeDto.getLevel());
+        recipe.setCookingTime(recipeDto.getCookingTime());
+        recipe.setServing(recipeDto.getServing());
+        recipe.setImage(recipeDto.getImage());
+        recipe.setCategory(recipeDto.getCategory());
 
-		// Meal 설정
-		Meal meal = new Meal();
-		meal.setId(recipeDto.getMeal().getId());
-		recipe.setMeal(meal);
+        // Meal 설정
+        Meal meal = new Meal();
+        meal.setId(recipeDto.getMeal().getId());
+        recipe.setMeal(meal);
 
-		// Member 설정
-		Member member = new Member();
-		member.setId(memberId);
-		recipe.setMember(member);
+        // Member 설정
+        Member member = new Member();
+        member.setId(memberId);
+        recipe.setMember(member);
 
-		// 레시피 저장
-		recipeRepository.save(recipe);
+        // **가격 계산 로직**
+        int totalPrice = 0;
 
-		// 2. RecipeIngredient 저장
-		for (RecipeIngredientDTO ingredientDto : recipeDto.getIngredients()) {
-			RecipeIngredient recipeIngredient = new RecipeIngredient();
-			Ingredient ingredient = new Ingredient();
-			ingredient.setId(ingredientDto.getIngredientId());
-			recipeIngredient.setIngredient(ingredient);
-			recipeIngredient.setRecipe(recipe);
-			recipeIngredient.setQuantity(ingredientDto.getQuantity());
-			recipeIngredientRepository.save(recipeIngredient);
-		}
+        // 2. RecipeIngredient 저장 및 가격 계산
+        for (RecipeIngredientDTO ingredientDto : recipeDto.getIngredients()) {
+            RecipeIngredient recipeIngredient = new RecipeIngredient();
+            Ingredient ingredient = ingredientRepository.findById(ingredientDto.getIngredientId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 재료 ID: " + ingredientDto.getIngredientId()));
 
-		// 3. RecipeStep 저장
-		for (RecipeStepDTO stepDto : recipeDto.getSteps()) {
-			RecipeStep recipeStep = new RecipeStep();
-			recipeStep.setRecipe(recipe);
-			recipeStep.setContent(stepDto.getContent());
-			recipeStep.setImage(stepDto.getImage());
-			recipeStepRepository.save(recipeStep);
-		}
+            int ingredientPrice = (int) ingredient.getStandardPrice(); // 재료 가격 가져오기
+            int quantity = ingredientDto.getQuantity();
+            
 
-		// 4. RecipeHashtag 저장
-		 List<Long> hashtagIds = recipeDto.getHashtagIds();
-		    if (hashtagIds == null) {
-		        hashtagIds = new ArrayList<>(); // null일 경우 빈 리스트로 처리
-		    }
-		for (Long hashtagId : hashtagIds) {
-			RecipeHashtag recipeHashtag = new RecipeHashtag();
-			Hashtag hashtag = new Hashtag();
-			hashtag.setId(hashtagId);
-			recipeHashtag.setRecipe(recipe);
-			recipeHashtag.setHashtag(hashtag);
-			recipeHashtagRepository.save(recipeHashtag);
-		}
-	}
+            totalPrice += ingredientPrice * quantity; // 가격 합산
+
+            recipeIngredient.setIngredient(ingredient);
+            recipeIngredient.setRecipe(recipe);
+            recipeIngredient.setQuantity(ingredientDto.getQuantity());
+            recipeIngredientRepository.save(recipeIngredient);
+        }
+
+        // 최종 price 설정 후 저장
+        recipe.setPrice(totalPrice);
+        recipeRepository.save(recipe);
+
+        // 3. RecipeStep 저장
+        for (RecipeStepDTO stepDto : recipeDto.getSteps()) {
+            RecipeStep recipeStep = new RecipeStep();
+            recipeStep.setRecipe(recipe);
+            recipeStep.setContent(stepDto.getContent());
+            recipeStep.setImage(stepDto.getImage());
+            recipeStepRepository.save(recipeStep);
+        }
+
+        // 4. RecipeHashtag 저장
+        List<Long> hashtagIds = recipeDto.getHashtagIds();
+        if (hashtagIds == null) {
+            hashtagIds = new ArrayList<>(); // null 방지
+        }
+        for (Long hashtagId : hashtagIds) {
+            RecipeHashtag recipeHashtag = new RecipeHashtag();
+            Hashtag hashtag = new Hashtag();
+            hashtag.setId(hashtagId);
+            recipeHashtag.setRecipe(recipe);
+            recipeHashtag.setHashtag(hashtag);
+            recipeHashtagRepository.save(recipeHashtag);
+        }
+    }
 }
--- backend/src/main/java/com/shinhan/naengtureat/recipe/vo/RecipeVO.java
+++ backend/src/main/java/com/shinhan/naengtureat/recipe/vo/RecipeVO.java
@@ -3,6 +3,7 @@ package com.shinhan.naengtureat.recipe.vo;
 import com.shinhan.naengtureat.member.entity.Member;
 import com.shinhan.naengtureat.recipe.dto.RecipeDTO;
 import com.shinhan.naengtureat.recipe.entity.Meal;
+import com.shinhan.naengtureat.recipe.entity.Recipe;
 
 import lombok.ToString;
 import lombok.Value;
@@ -41,5 +42,20 @@ public class RecipeVO {
         this.image = dto.getImage();
         this.category = dto.getCategory();
         this.isDelete = dto.getIsDelete();
+    }
+    
+    // Recipe 객체를 받아서 VO를 생성하는 생성자
+    public RecipeVO(Recipe recipe) {
+        this.id = recipe.getId();
+        this.meal = recipe.getMeal();
+        this.member = recipe.getMember();
+        this.name = recipe.getName();
+        this.price = recipe.getPrice();
+        this.level = recipe.getLevel();
+        this.cookingTime = recipe.getCookingTime();
+        this.serving = recipe.getServing();
+        this.image = recipe.getImage();
+        this.category = recipe.getCategory();
+        this.isDelete = recipe.getIsDelete();
     }
 }
