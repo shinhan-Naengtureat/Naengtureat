693ab21c0e691196722149b4c6fcb65d0e5dd609
Lee Taehan <kpl2955@gmail.com>
2025-03-06 17:05:03 +0900

    feat 테마(키워드) 조회 기능 추가
    Merge branch 'develop' of
    https://github.com/shinhan-Naengtureat/Naengtureat.git into
    feat/#58-CategoryGet

--- backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
+++ backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
@@ -1,8 +1,13 @@
 package com.shinhan.naengtureat.mealplan;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +26,26 @@ public class MealPlanController {
 
 	// 카테고리 조회
 	@GetMapping("/category")
-	public List<String> getCategory() {
-		return mealPlanService.getCategoryAll();
+	public ResponseEntity<Object> getCategory() {
+			List<String> category = mealPlanService.getCategoryAll();
+			if (category == null || category.isEmpty()) {
+				throw new NoSuchElementException("해당 카테고리를 찾을 수 없습니다.");
+			}
+			return ResponseEntity.ok(category);
+
+		
 	}
 
+	// 테마조회
+	@GetMapping("/theme")
+	public ResponseEntity<Object> getTheme() {
+			List<String> theme = mealPlanService.getThemeAll();
+			if (theme == null || theme.isEmpty()) {
+				throw new NoSuchElementException("해당 테마를 찾을 수 없습니다.");
+			}
+			return ResponseEntity.ok(theme);
+
+		
+
+	}
 }
--- backend/src/main/java/com/shinhan/naengtureat/mealplan/model/MealPlanService.java
+++ backend/src/main/java/com/shinhan/naengtureat/mealplan/model/MealPlanService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shinhan.naengtureat.recipe.model.RecipeHashtagRepository;
 import com.shinhan.naengtureat.recipe.model.RecipeRepository;
 
 @Service
@@ -12,9 +13,16 @@ public class MealPlanService {
 
 	@Autowired
 	RecipeRepository recipeRepo;
+	
+	@Autowired
+	RecipeHashtagRepository recipeHashRepo;
 
 	public List<String> getCategoryAll() {
 		return recipeRepo.findCategoryAll();
 	}
 
+	public List<String> getThemeAll() {
+		return recipeHashRepo.findThemeAll();
+	}
+	
 }
