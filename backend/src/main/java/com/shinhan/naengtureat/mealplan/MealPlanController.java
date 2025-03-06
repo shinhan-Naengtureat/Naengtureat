a7e8ac0a33cbb8fc849a757928dd563e7b3f8ac7
Lee Taehan <kpl2955@gmail.com>
2025-03-06 17:06:42 +0900

    feat 카테고리 조회 / 에러처리방식 변경
    에러처리 코드 변경

--- backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
+++ backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
@@ -1,13 +1,10 @@
 package com.shinhan.naengtureat.mealplan;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,23 +21,17 @@ public class MealPlanController {
 	@Autowired
 	MealPlanService mealPlanService;
 
+
 	// 카테고리 조회
 	@GetMapping("/category")
 	public ResponseEntity<Object> getCategory() {
-		try {
 			List<String> category = mealPlanService.getCategoryAll();
 			if (category == null || category.isEmpty()) {
-				return ResponseEntity.noContent().build();
+				throw new NoSuchElementException("해당 카테고리를 찾을 수 없습니다.");
 			}
 			return ResponseEntity.ok(category);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			Map<String, String> errorResponse = new HashMap<>();
-			errorResponse.put("error", "카테고리 조회 중 오류 발생");
-			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-		}
+		
 	}
 
 }
