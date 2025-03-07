3bbc7edbcfa74d8522d119dd5a3a27e44b0fb057
jamm0316 <jamm0316@naver.com>
2025-03-08 01:45:27 +0900

    feat: 인벤토리 수정 기능
    Resolves #89

--- backend/src/test/java/com/shinhan/naengtureat/InventoryCRUDTest.java
+++ backend/src/test/java/com/shinhan/naengtureat/InventoryCRUDTest.java
@@ -11,8 +11,12 @@ import com.shinhan.naengtureat.member.model.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,8 +37,6 @@ public class InventoryCRUDTest {
         //given
         Long ingredientId = 92L;
         Long memberId = 1L;
-        Member memberById = memberService.getMemberById(memberId);
-
 
         InventoryDTO inventoryDTO = InventoryDTO.builder()
                 .id(2L)
@@ -42,23 +44,31 @@ public class InventoryCRUDTest {
                 .inventoryExpDate(LocalDate.parse("2025-03-15"))
                 .inputDate(LocalDate.parse("2025-03-15"))
                 .quantity(3)
-                .nickName("완전 맛없는 앞다리살")
-                .member(memberById)
+                .nickName("으억 내가 먹을거야")
+                .memberId(memberId)
                 .build();
 
         //when
+        if (inventoryDTO.getQuantity() <= 0) {
+            throw new IllegalArgumentException("재료 수량은 0 이상 이여야 합니다.");
+        }
+        // 기존 재고 조회
+        Inventory inventory = inventoryRepository.findById(inventoryDTO.getId())
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 재료 입니다."));
 
-        Ingredient ingredient = ingredientService.getStandardIngredientById(ingredientId);
-        inventoryDTO.setIngredient(ingredient);
-        Inventory inventory = inventoryService.convertEntity(inventoryDTO);
+        //재료 검증
+        Ingredient ingredient = ingredientService.getStandardIngredientById(inventoryDTO.getIngredientId());
 
-        inventoryRepository.save(inventory);
+        inventory.setQuantity(inventoryDTO.getQuantity());  //변경된 수량 등록
+        inventory.setNickName(inventoryDTO.getNickName());  //변경된 닉네임 등록
+        inventory.setMemo(inventoryDTO.getMemo());  //변경된 메모 등록
+        inventory.setInventoryExpDate(inventoryDTO.getInventoryExpDate());  //변경된 유효기간 등록
+        inventory.setInputDate(inventoryDTO.getInputDate());  //변경된 인입일 등록
+        inventory.setIngredient(ingredient);  // 유효한 재료 등록
 
         //then
         Inventory newInventory = inventoryRepository.findById(2L).orElse(null);
-
-        assertEquals(newInventory.getNickName(), "완전 맛없는 앞다리살");
+        assertEquals(newInventory.getNickName(), "으억 내가 먹을거야");
         assertEquals(newInventory.getMember().getId(), 1L);
-
     }
 }
