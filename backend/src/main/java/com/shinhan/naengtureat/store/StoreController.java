6bf3612ac937a7a5ca886b45ddec54caae037e2f
GJKim-James <gjk0635@naver.com>
2025-03-07 15:38:00 +0900

    feat: 스토어 상세 조회
    #38 코드 리팩토링하러 가기 위한 커밋
    
    작업 중인 issue #81

--- backend/src/main/java/com/shinhan/naengtureat/store/StoreController.java
+++ backend/src/main/java/com/shinhan/naengtureat/store/StoreController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shinhan.naengtureat.store.dto.StoreDTO;
 import com.shinhan.naengtureat.store.dto.StoreReviewDTO;
 import com.shinhan.naengtureat.store.entity.Store;
 import com.shinhan.naengtureat.store.model.StoreReviewService;
+import com.shinhan.naengtureat.store.model.StoreService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,10 +27,11 @@ import lombok.extern.slf4j.Slf4j;
 public class StoreController {
 	
 	private final StoreReviewService storeReviewService;
+	private final StoreService storeService;
 	
 	// 스토어 후기 조회
-	@GetMapping("/{store_id}/review")
-	public ResponseEntity<Object> getStoreReviewByStoreId(@PathVariable("store_id") Long storeId) {
+	@GetMapping("/{storeId}/review")
+	public ResponseEntity<Object> getStoreReviewByStoreId(@PathVariable("storeId") Long storeId) {
 		
 		try {
 			Store storeEntity = Store.builder().id(storeId).build();
@@ -40,6 +43,25 @@ public class StoreController {
 			e.printStackTrace();
 			Map<String, String> errorResponse = new HashMap<>();
 			errorResponse.put("error", "스토어 후기 조회 중 오류 발생");
+			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+		}
+		
+	}
+	
+	// 스토어 상세 조회
+	@GetMapping("/{storeId}/detail")
+	public ResponseEntity<Object> getStoreDetailByStoreId(@PathVariable("storeId") Long storeId) {
+		
+		try {
+			StoreDTO storeDTO = storeService.getDetailByStore(storeId);
+			
+			return ResponseEntity.ok(storeDTO);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Map<String, String> errorResponse = new HashMap<>();
+			errorResponse.put("error", "스토어 상세 조회 중 오류 발생");
 			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
 
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
--- backend/src/main/java/com/shinhan/naengtureat/store/model/StoreService.java
+++ backend/src/main/java/com/shinhan/naengtureat/store/model/StoreService.java
@@ -1,12 +1,33 @@
 package com.shinhan.naengtureat.store.model;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.shinhan.naengtureat.store.dto.StoreDTO;
+import com.shinhan.naengtureat.store.entity.Store;
 
 @Service
 public class StoreService {
 	
 	@Autowired
 	StoreRepository storeRepository;
+
+	// 스토어 상세 조회
+	public StoreDTO getDetailByStore(Long storeId) {
+		Store storeDetail = storeRepository.findById(storeId).orElse(null);
+		
+		// Entity를 DTO로 변환 후 리턴
+		return entityToDTO(storeDetail);
+	}
+	
+	// Entity를 DTO로 변환(Data 전송을 위함, controller, service, view에서 작업)
+	// 조회 시 사용
+	public StoreDTO entityToDTO(Store entity) {
+		ModelMapper mapper = new ModelMapper();
+		StoreDTO dto = mapper.map(entity, StoreDTO.class); // 이름이 같은 필드들은 자동으로 매핑
+		
+		return dto;
+	}
 
 }
