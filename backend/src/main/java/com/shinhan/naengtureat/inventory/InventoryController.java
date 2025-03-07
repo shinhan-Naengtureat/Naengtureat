59ce1cbc1999c8ddeb16a294c1e88ff7052cc992
jamm0316 <jamm0316@naver.com>
2025-03-07 10:14:06 +0900

    feat: 인벤토리 재료 삭제
    Resolves #77

--- backend/src/main/java/com/shinhan/naengtureat/inventory/InventoryController.java
+++ backend/src/main/java/com/shinhan/naengtureat/inventory/InventoryController.java
@@ -11,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -52,5 +49,12 @@ public class InventoryController {
 
         String resultMessage = inventoryService.createInventory(inventoryDTO);
         return ResponseEntity.ok(resultMessage);
+    }
+
+    @DeleteMapping("/{inventoryId}")
+    public ResponseEntity<Object> deleteInventory(@PathVariable("inventoryId") Long inventoryId) {
+        InventoryDTO inventoryDTO = inventoryService.getInventoryById(inventoryId);
+        String result = inventoryService.deleteInventory(inventoryDTO);
+        return ResponseEntity.ok(result);
     }
 }
--- backend/src/main/java/com/shinhan/naengtureat/inventory/model/InventoryService.java
+++ backend/src/main/java/com/shinhan/naengtureat/inventory/model/InventoryService.java
@@ -7,19 +7,33 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class InventoryService {
     @Autowired
     InventoryRepository inventoryRepository;
 
+    public InventoryDTO getInventoryById(Long inventoryId) {
+        Inventory inventory = inventoryRepository.findById(inventoryId)
+                .orElseThrow(() -> new NoSuchElementException("해당 재료를 찾을 수 없습니다."));
+        return convertDto(inventory);
+    }
+
     public String createInventory(InventoryDTO inventoryDTO) {
         if (inventoryDTO.getQuantity() <= 0) {
-            throw new IllegalArgumentException("인벤토리 수량은 0이상 이여야 합니다.");
+            throw new IllegalArgumentException("재료 수량은 0이상 이여야 합니다.");
         }
 
         inventoryRepository.save(convertEntity(inventoryDTO));
-        return "인벤토리 저장이 완료되었습니다.";
+        return "재료 저장이 완료되었습니다.";
+    }
+
+    public String deleteInventory(InventoryDTO inventoryDTO) {
+        inventoryRepository.delete(convertEntity(inventoryDTO));
+        return "재료 삭제가 완료되었습니다.";
     }
 
     public InventoryDTO convertDto(Inventory inventory) {
