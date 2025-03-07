f51c7f3f3ba2c76f2071ded31da7f0f10417b267
jamm0316 <jamm0316@naver.com>
2025-03-08 03:06:31 +0900

    feat: 내 인벤토리 모든 재료 조회
    1. 모든 재료 조회 기능 추가
    2. 등록하는 DTO 필드와 반환하는 DTO의 필드가 다름
      - 따라서, RequestDTO, ResponseDTO를 나누어 작성
    
    Resolves #104

--- backend/src/main/java/com/shinhan/naengtureat/inventory/InventoryController.java
+++ backend/src/main/java/com/shinhan/naengtureat/inventory/InventoryController.java
@@ -1,25 +1,17 @@
 package com.shinhan.naengtureat.inventory;
 
 import com.shinhan.naengtureat.ingredient.dto.IngredientComparisonDTO;
-
 import com.shinhan.naengtureat.inventory.dto.InventoryRequestDTO;
 import com.shinhan.naengtureat.inventory.dto.InventoryResponseDTO;
 import com.shinhan.naengtureat.inventory.model.InventoryService;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @RestController
