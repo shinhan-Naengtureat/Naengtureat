ee8af8923eea73284b7c1c6022d2acee804798d9
eunziCode <eunzi.code@gmail.com>
2025-03-07 16:02:41 +0900

    feat: 식단 월간 조회 api 개발
    로그인한 유저가 식단 월간 조회했을때 데이터 나오게 개발
    
    Resolves #83

--- backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
+++ backend/src/main/java/com/shinhan/naengtureat/mealplan/MealPlanController.java
@@ -88,4 +88,23 @@ public class MealPlanController {
 		return ResponseEntity.ok(updatedRows);
 	}
 
+	// 식단 월간 조회
+	@GetMapping("/month/{month}")
+	public ResponseEntity<Object> getMonthlyMealPlanList(@PathVariable("month") String month) {
+		try {
+			// 세션에서 로그인된 사용자 정보 가져오기
+			Long memberId = 2L; // security 적용시 코드 수정 필요(WebBoardController SecurityContextHolder, MemberService 참고)
+
+			List<MealPlanDTO> monthlyMealPlanList = mealPlanService.getMonthlyMealPlanList(memberId, month);
+
+			return ResponseEntity.ok(monthlyMealPlanList);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Map<String, String> errorResponse = new HashMap<>();
+			errorResponse.put("error", "식단 월간 조회 중 오류 발생");
+			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+		}
+	}
 }
