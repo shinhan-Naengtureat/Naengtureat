b6c1138774fb7d907c2ceee1f2784853977a2e2e
jamm0316 <jamm0316@naver.com>
2025-03-10 15:28:54 +0900

    feat: 리엑트 프론트엔드 구조 잡기
    1. Router 분리
    2. 아이폰 13pro size로 해상도 조정
    
    Resolves #124

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Router, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import HomePage from "pages/home/HomePage";
 import RecipeList from "pages/recipe/RecipeList";
 import InventoryList from "pages/inventory/InventoryList";
