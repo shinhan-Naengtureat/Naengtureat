145faef45d550d6a8f8b00a94f2471b2e5d0567d
GJKim-James <gjk0635@naver.com>
2025-03-10 11:27:36 +0900

    feat: 장바구니 추가
    특정 스토어에서 상품에 표시된 + 아이콘 클릭 시 로그인 한 사용자의 장바구니에 해당 재료(상품)를 추가
    
    Resolves #126

--- backend/src/main/java/com/shinhan/naengtureat/store/StoreController.java
+++ backend/src/main/java/com/shinhan/naengtureat/store/StoreController.java
@@ -24,6 +24,9 @@ import com.shinhan.naengtureat.store.model.StoreService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Slf4j
 @RestController
@@ -156,5 +159,28 @@ public class StoreController {
 		}
 		
 	}
+	
+	// 장바구니 추가
+	@PostMapping("/cart/{productId}")
+	public ResponseEntity<Object> addCart(@PathVariable("productId") Long productId, @RequestBody StoreProductDTO storeProductDTO) {
+		
+		try {
+			// 세션에서 로그인된 사용자 정보 가져오기
+			Long memberId = 2L; // security 적용시 코드 수정 필요(WebBoardController SecurityContextHolder, MemberService 참고)
+			
+			Map<String, Object> result = storeCartService.addCartItem(memberId, productId, storeProductDTO);
+			
+			return ResponseEntity.ok(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Map<String, String> errorResponse = new HashMap<>();
+			errorResponse.put("error", "스토어 장바구니 추가 중 오류 발생");
+			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+		}
+		
+	}
+	
 
 }
--- backend/src/main/java/com/shinhan/naengtureat/store/model/StoreCartService.java
+++ backend/src/main/java/com/shinhan/naengtureat/store/model/StoreCartService.java
@@ -1,11 +1,17 @@
 package com.shinhan.naengtureat.store.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shinhan.naengtureat.member.dto.CartDTO;
+import com.shinhan.naengtureat.member.entity.Cart;
+import com.shinhan.naengtureat.member.entity.Member;
+import com.shinhan.naengtureat.store.dto.StoreProductDTO;
+import com.shinhan.naengtureat.store.entity.StoreProduct;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +28,37 @@ public class StoreCartService {
 		
 		// Entity를 DTO로 변환 후 List 형태로 리턴
 		return cartDTOList;
+	}
+
+	// 장바구니에 재료(상품) 추가
+	public Map<String, Object> addCartItem(Long memberId, Long productId, StoreProductDTO storeProductDTO) {
+		// 로그인 한 사용자의 장바구니에 추가하기 위해
+		Member member = Member.builder().id(memberId).build();
+		
+		// 어떤 재료(상품)를 장바구니에 추가할 것인지
+		StoreProduct storeProduct = StoreProduct.builder().id(productId).build();
+		
+		// 로그인 한 사용자의 장바구니에 재료(상품) 추가
+		Cart cartEntity = Cart.builder().count(1)
+										.isCheck(false)
+										.member(member)
+										.product(storeProduct).build();
+		Cart savedCart = storeCartRepository.save(cartEntity);
+		
+		// 장바구니 추가 성공 여부에 대한 결과 문구
+		String message = "";
+		if (savedCart != null) {
+			message = "해당 상품이 장바구니에 추가되었습니다.";
+		} else {
+			message = "장바구니 추가에 실패했습니다.";
+		}
+		
+		// 장바구니 추가 성공 여부에 대한 결과 문구와 추가된 내역 정보(savedCart) 리턴
+		Map<String, Object> response = new HashMap<>();
+		response.put("savedCart", savedCart);
+		response.put("message", message);
+		
+		return response;
 	}
 
 }
