9f1953e8d6b8e011d583989d8122131d98e8b07c
jamm0316 <jamm0316@naver.com>
2025-03-11 04:17:34 +0900

    feat: 동적 인벤토리 검색 기능 추가
    keyword를 받아서 아래와 같은 조건으로 동적 검색
    1. smallCategory
    2. bigCategory
    3. nickName
    4. memo
    
    Resolves #123

--- backend/src/main/java/com/shinhan/naengtureat/common/config/QueryDslConfig.java
+++ backend/src/main/java/com/shinhan/naengtureat/common/config/QueryDslConfig.java
@@ -1,6 +1,5 @@
 package com.shinhan.naengtureat.common.config;
 
-import com.querydsl.core.annotations.Config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
