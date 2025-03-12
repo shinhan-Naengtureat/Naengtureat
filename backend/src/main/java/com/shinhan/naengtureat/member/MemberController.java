b95d7c041ac764cb67df28a16c53db1b379926c6
GJKim-James <gjk0635@naver.com>
2025-03-12 19:45:19 +0900

    feat: 로그인 한 사용자 정보 조회
    카카오맵 API 호출 시 로그인 한 사용자의 도로명 주소를 좌표로 변환하기 위한 사용자 정보 조회 기능
    
    Resolves #185

--- backend/src/main/java/com/shinhan/naengtureat/member/MemberController.java
+++ backend/src/main/java/com/shinhan/naengtureat/member/MemberController.java
@@ -1,7 +1,18 @@
 package com.shinhan.naengtureat.member;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shinhan.naengtureat.member.dto.MemberDTO;
+import com.shinhan.naengtureat.member.model.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,5 +20,30 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/member")
 public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	// 사용자 정보 조회
+	@GetMapping("/detail")
+	public ResponseEntity<Object> getMemberDetail() {
+		
+		try {
+			// 세션에서 로그인된 사용자 정보 가져오기
+			Long memberId = 3L; // security 적용시 코드 수정 필요(WebBoardController SecurityContextHolder, MemberService 참고)
+			
+			MemberDTO memberDTO = memberService.getMemberById(memberId);
+			
+			return ResponseEntity.ok(memberDTO);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Map<String, String> errorResponse = new HashMap<>();
+			errorResponse.put("error", "스토어 후기 조회 중 오류 발생");
+			errorResponse.put("message", e.getMessage()); // 예외 메시지 포함
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+		}
+		
+	}
 
 }
