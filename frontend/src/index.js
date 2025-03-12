b7f90673adcaa2a855b3cb9e3e9b77cd37ff2fc1
jamm0316 <jamm0316@naver.com>
2025-03-13 08:17:47 +0900

    feat: 부트스트랩 css 추가
    Resolves #190

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from "routes/AppRouter";
 import {BrowserRouter} from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
