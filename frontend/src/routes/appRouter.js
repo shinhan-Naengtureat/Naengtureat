c948af9f1a656d803c4d2d5dfe0f3ffa6e637879
Woong0405 <dbstjddnd45@gmail.com>
2025-03-13 16:04:45 +0900

    refactor: front되돌리기

--- frontend/src/routes/appRouter.js
+++ frontend/src/routes/appRouter.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
 import HomePage from "pages/home/HomePage";
 import RecipeList from "pages/recipe/RecipeList";
 import InventoryList from "pages/inventory/InventoryList";
@@ -13,72 +13,25 @@ import BottomNav from "components/common/BottomNav";
 const AppRouter = () => {
   return (
     <div className="app">
-      <TopNav />
-      <Routes>
-        <Route
-          path={routeConfig.paths.home}
-          element={
-            <Layout>
-              <HomePage />
-            </Layout>
-          }
-        />
-        <Route
-          path={routeConfig.paths.inventoryList}
-          element={
-            <Layout>
-              <InventoryList />
-            </Layout>
-          }
-        />
-        <Route
-          path={routeConfig.paths.mealPlanListDaily}
-          element={
-            <Layout>
-              <MealPlanListDaily />
-            </Layout>
-          }
-        />
-        <Route
-          path={routeConfig.paths.recipeList}
-          element={
-            <Layout>
-              <RecipeList />
-            </Layout>
-          }
-        />
-        <Route
-          path={routeConfig.paths.storeList}
-          element={
-            <Layout>
-              <StoreList />
-            </Layout>
-          }
-        />
+      <TopNav/>
+        <Routes>
+          <Route path={routeConfig.paths.home} element={<Layout><HomePage/></Layout>} />
+          <Route path={routeConfig.paths.inventoryList} element={<Layout><InventoryList/></Layout>} />
+          <Route path={routeConfig.paths.mealPlanListDaily} element={<Layout><MealPlanListDaily/></Layout>} />
+          <Route path={routeConfig.paths.recipeList} element={<Layout><RecipeList/></Layout>} />
+          <Route path={routeConfig.paths.storeList} element={<Layout><StoreList/></Layout>} />
 
-        {/* 필요한 경우 추가할 수 있는 라우트 */}
-        <Route
-          path={routeConfig.paths.login}
-          element={<Layout>Login Page</Layout>}
-        />
-        <Route
-          path={routeConfig.paths.register}
-          element={<Layout>Register Page</Layout>}
-        />
-        <Route
-          path={routeConfig.paths.myPage}
-          element={<Layout>MyPage</Layout>}
-        />
+          {/* 필요한 경우 추가할 수 있는 라우트 */}
+          <Route path={routeConfig.paths.login} element={<Layout>Login Page</Layout>} />
+          <Route path={routeConfig.paths.register} element={<Layout>Register Page</Layout>} />
+          <Route path={routeConfig.paths.myPage} element={<Layout>MyPage</Layout>} />
 
-        {/* 404 Not Found */}
-        <Route
-          path={routeConfig.paths.notFound}
-          element={<h1>404 Not Found</h1>}
-        />
-      </Routes>
-      <BottomNav />
+          {/* 404 Not Found */}
+          <Route path={routeConfig.paths.notFound} element={<h1>404 Not Found</h1>} />
+        </Routes>
+      <BottomNav/>
     </div>
   );
-};
+}
 
 export default AppRouter;
