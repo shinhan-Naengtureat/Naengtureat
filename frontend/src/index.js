8ca427f97836f0343a0650933f9202d750dd770f
jamm0316 <jamm0316@naver.com>
2025-03-13 10:29:57 +0900

    design: topNav, home-container 수정
    Resolves #195

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppRouter from "routes/AppRouter";
+import AppRouter from "routes/appRouter";
 import {BrowserRouter} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
