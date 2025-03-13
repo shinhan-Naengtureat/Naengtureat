cedf6246f16af49795652edbb912e9719e2a33d2
jamm0316 <jamm0316@naver.com>
2025-03-13 18:02:37 +0900

    refactor: inventoryResponseDTO field 추가
    String ingredientBigCategory 필드 추가
    
    Resolves #205

--- backend/src/main/java/com/shinhan/naengtureat/inventory/dto/InventoryResponseDTO.java
+++ backend/src/main/java/com/shinhan/naengtureat/inventory/dto/InventoryResponseDTO.java
@@ -21,5 +21,6 @@ public class InventoryResponseDTO {
     private Long memberId;  //멤버 아이디
     private Long ingredientId;  //재료 아이디
     private String ingredientName;  //재료 이름
+    private String ingredientBigCategory;  //대분류
     private int remainingDays;  //소비기한 D-day값
 }
