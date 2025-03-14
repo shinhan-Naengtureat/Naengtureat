2f01be8a0c227b95af4cffaf29002b41f9912fe1
jamm0316 <jamm0316@naver.com>
2025-03-14 15:32:19 +0900

    feat: ingredient big, smallCategory 모두 가져오기
    Resolves #225

--- backend/src/main/java/com/shinhan/naengtureat/ingredient/IngredientController.java
+++ backend/src/main/java/com/shinhan/naengtureat/ingredient/IngredientController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequestMapping("/ingredient")
