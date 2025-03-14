f9f79305003ddd282d6b1e24387bee9487860b41
Woong0405 <dbstjddnd45@gmail.com>
2025-03-14 09:52:13 +0900

    feat: 전체 레시피 조회 페이지
    전체 레시피 조회, 레시피 정렬필터, 식재료 필터(대분류), 레시피 카테고리 필터, 레시피 검색, 레시피 관련 css작성
    
    Resolves #197

--- frontend/src/routes/appRouter.js
+++ frontend/src/routes/appRouter.js
@@ -9,6 +9,7 @@ import routeConfig from "routes/routeConfig";
 import Layout from "components/common/Layout";
 import TopNav from "components/common/TopNav";
 import BottomNav from "components/common/BottomNav";
+import RecipeSearchPage from "pages/recipe/RecipeSearchPage";
 
 const AppRouter = () => {
   return (
@@ -25,6 +26,7 @@ const AppRouter = () => {
           <Route path={routeConfig.paths.login} element={<Layout>Login Page</Layout>} />
           <Route path={routeConfig.paths.register} element={<Layout>Register Page</Layout>} />
           <Route path={routeConfig.paths.myPage} element={<Layout>MyPage</Layout>} />
+          <Route path="/search-recipe" element={<Layout><RecipeSearchPage/></Layout>} />
 
           {/* 404 Not Found */}
           <Route path={routeConfig.paths.notFound} element={<h1>404 Not Found</h1>} />
