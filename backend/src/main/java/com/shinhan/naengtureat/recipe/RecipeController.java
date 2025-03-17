caafefc4d4a1ee73caaad6c73187f762aff1e903
jamm0316 <jamm0316@naver.com>
2025-03-17 12:21:42 +0900

    refactor: memberId 전체 변경
    사용자 memberId 모두 3L로 변경
    
    [참조]
    1. admin 1L
    2. user 2L(dummy)
    ** 3. user 3L(Test) **
    
    Resolves #244

--- backend/src/main/java/com/shinhan/naengtureat/recipe/RecipeController.java
+++ backend/src/main/java/com/shinhan/naengtureat/recipe/RecipeController.java
@@ -1,23 +1,5 @@
 package com.shinhan.naengtureat.recipe;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.shinhan.naengtureat.common.response.BaseResponse;
 import com.shinhan.naengtureat.mealplan.dto.MealPlanCheckDTO;
 import com.shinhan.naengtureat.mealplan.dto.MealPlanDTO;
@@ -28,8 +10,14 @@ import com.shinhan.naengtureat.recipe.dto.RecipeDetailDTO;
 import com.shinhan.naengtureat.recipe.entity.Likes;
 import com.shinhan.naengtureat.recipe.model.LikesService;
 import com.shinhan.naengtureat.recipe.model.RecipeService;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
