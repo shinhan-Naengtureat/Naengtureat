cfb7eec5874e6866e307698de34d7790f1500551
jamm0316 <jamm0316@naver.com>
2025-03-17 22:01:34 +0900

    refactor: dev pull 이후 수정
    appRouter pull 후 수정
    
    Resolves #263

--- frontend/src/routes/appRouter.js
+++ frontend/src/routes/appRouter.js
@@ -24,6 +24,7 @@ import StoreReviewDetail from 'pages/store/StoreReviewDetail';
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import routeConfig from "routes/routeConfig";
+import InventoryWastebucket from "pages/inventory/InventoryWastebucket";
 
 const AppRouter = () => {
   const [userSelections, setUserSelections] = useState({
@@ -45,6 +46,7 @@ const AppRouter = () => {
         <Route path={routeConfig.paths.inventoryList} element={<Layout> <InventoryList /></Layout>}/>
         <Route path={routeConfig.paths.inventoryDetail} element={<Layout><InventoryDetail/></Layout>} />
         <Route path={routeConfig.paths.inventoryCreate} element={<Layout><InventoryCreate/></Layout>} />
+        <Route path={routeConfig.paths.inventoryWastebucket} element={<Layout><InventoryWastebucket/></Layout>} />
 
         {/*mealPlan*/}
         <Route path={routeConfig.paths.mealPlanListDaily} element={ <Layout><MealPlanListDaily userSelections={userSelections} /></Layout>}/>
