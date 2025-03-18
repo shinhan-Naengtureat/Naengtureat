18563bbd8d3962ffc46956a27ce1b86d1f693fbc
eunziCode <eunzi.code@gmail.com>
2025-03-18 10:27:47 +0900

    feat: 로그인한 유저의 pay정보 받아오기
    유저 pay정보 받아오는 기능 개발
    
    Resolves #284

--- backend/src/main/java/com/shinhan/naengtureat/pay/PayController.java
+++ backend/src/main/java/com/shinhan/naengtureat/pay/PayController.java
@@ -1,7 +1,15 @@
 package com.shinhan.naengtureat.pay;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shinhan.naengtureat.pay.dto.PayDTO;
+import com.shinhan.naengtureat.pay.model.PayService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,5 +17,21 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/pay")
 public class PayController {
+	
+	@Autowired
+	PayService payService;
+	
+	// 로그인한 유저의 pay정보 받아오기
+	@GetMapping
+	public ResponseEntity<Object> getPay() {
+		Long memberId = 3L; // security 적용시 코드 수정 필요(WebBoardController SecurityContextHolder, MemberService 참고)
+		
+		PayDTO pay = payService.getPayByMember(memberId);
+		
+		if (pay == null) {
+			throw new NoSuchElementException("로그인한 유저 정보를 확인할 수 없습니다.");
+		}
+		return ResponseEntity.ok(pay);
+	}
 
 }
