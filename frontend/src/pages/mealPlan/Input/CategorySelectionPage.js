976e3f82bc0f50113fa4f8489ffc77d44d3d222e
Lee Taehan <kpl2955@gmail.com>
2025-03-19 09:29:57 +0900

    Merge branch 'develop' of https://github.com/shinhan-Naengtureat/Naengtureat.git into feat/#318-save-mealplan

--- frontend/src/pages/mealPlan/Input/CategorySelectionPage.js
+++ frontend/src/pages/mealPlan/Input/CategorySelectionPage.js
@@ -67,15 +67,15 @@ const CategorySelectionPage = () => {
   };
 
   return (
-    <div className="category-container">
+    <div className="box-container">
       {/* 뒤로가기 & 타이틀 */}
-      <div className="category-header">
+      <div className="box-header">
         <BackButton onClick={handleBefore} />
       </div>
 
-      <h2 className="category-title" style={{textAlign:"center"}}>
+      <h2 className="box-title" style={{textAlign:"center"}}>
         식단표에 추가하고 싶은 <br />
-        <span className="category-highlight">카테고리를 모두 선택하세요</span>
+        <span className="box-highlight">카테고리를 모두 선택하세요</span>
       </h2>
 
       {/*카테고리 버튼 목록 */}
