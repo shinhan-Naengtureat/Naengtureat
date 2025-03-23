d5efc7b0709f2a3386d65e912ce858b03a1b111c
GJKim-James <gjk0635@naver.com>
2025-03-23 15:27:28 +0900

    design: 스토어 화면 수정
    - 로딩 시 '~하는 중' 안내 메시지를 텍스트 형태에서 스피너 형태로 수정
    - 스토어 상품 추가(+ 버튼) 시 동적인 효과 적용
    
    Resolves #448

--- frontend/src/config/pathConfig.js
+++ frontend/src/config/pathConfig.js
@@ -1,7 +1,7 @@
 export const API_PATH = 'http://localhost:8888';
+export const ICON_IMAGE_PATH = '/assets/images/icons';
 export const INGREDIENT_IMAGE_PATH = '/assets/images/ingredients';
+export const PROFILE_IMAGE_PATH = '/assets/images/profiles';
 export const RECIPE_IMAGE_PATH = '/assets/images/recipes';
 export const STORE_IMAGE_PATH = '/assets/images/store';
-export const ICON_IMAGE_PATH = '/assets/images/icons';
-export const PROFILE_IMAGE_PATH = '/assets/images/profiles';
 export const FRONT_API_PATH = 'http://localhost:3000';
