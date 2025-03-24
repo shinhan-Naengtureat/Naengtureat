46c1895e8f7e5fa4441b0ee2ace4f01d4caf9ca9
jamm0316 <jamm0316@naver.com>
2025-03-24 10:34:22 +0900

    fix: 시간까지 조정
    Resolves #471

--- frontend/src/pages/inventory/InventoryCreate.jsx
+++ frontend/src/pages/inventory/InventoryCreate.jsx
@@ -29,9 +29,9 @@ const InventoryCreate = () => {
 
   // 오늘 날짜 가져오기 함수
   const getTodayDate = () => {
-    const today = new Date();
-    today.setMinutes(today.getMinutes() - today.getTimezoneOffset()); // 로컬 시간 보정
-    return today.toISOString().split("T")[0];
+      const now = new Date();
+      now.setHours(now.getHours() + 9); // UTC -> KST 변환
+      return now.toISOString().split("T")[0];
   };
 
   // 대분류 및 소분류 데이터 가져오기
